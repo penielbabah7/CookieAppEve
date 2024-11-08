e82054ba15651db509e6814389843ea76ab31a23
Daniel Baroi <danielbaroi@cedarville.edu>
2024-11-08 14:37:25 -0500

    Initial Commit

---
+++ CookieApp/CookieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookieAppApp.swift
+//  CookieApp
+//
+//  Created by Daniel Baroi on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CookieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
